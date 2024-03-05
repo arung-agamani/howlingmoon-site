d092e7d7269a1c831c55f57f21a33e0149c53bff
Arung Agamani <arung.agamani@gmail.com>
2024-03-05 17:17:20 +0800

    chore: disable particles, init lottie
    new project entries were added but still need more info
    tsparticles were disabled due to performance overhead
    switch to lottiefiles as im going to make motion graphics intro

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import Experiences from "./components/pages/Experiences";
 import Projects from "./components/pages/Projects";
 
 function App() {
-    const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-        await loadFull(engine);
-    }, []);
+  // const particlesInit = useCallback(async (engine: Engine) => {
+  //     console.log(engine);
+  //     await loadFull(engine);
+  // }, []);
 
-    const particlesLoaded = useCallback(
-        async (container: Container | undefined) => {
-            await console.log(container);
-        },
-        []
-    );
+  // const particlesLoaded = useCallback(
+  //     async (container: Container | undefined) => {
+  //         await console.log(container);
+  //     },
+  //     []
+  // );
 
-    return (
-        <div className="text-gray-400 bg-gray-900 bg-opacity-5 pb-32">
-            <Particles
+  return (
+    <div className="text-gray-400 bg-gray-900 bg-opacity-100 pb-32">
+      {/* <Particles
                 className="absolute -z-10"
                 id="tsparticles"
                 init={particlesInit}
@@ -65,13 +65,13 @@ function App() {
                         },
                     },
                 }}
-            />
-            <About />
-            <Projects />
-            <Experiences />
-            <Contacts />
-        </div>
-    );
+            /> */}
+      <About />
+      <Projects />
+      <Experiences />
+      <Contacts />
+    </div>
+  );
 }
 
 export default App;
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,20 +1,26 @@
-const About: React.FC = () => {
-    return (
-        <div className="container mx-auto flex flex-col justify-cente py-20 lg:py-40 px-10 h-screen">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-white">
-                Hi, My name is Arung.
-                <br/>I make stuffs. Various stuffs.
-              </h1>
-              <p className="mb-8 leading-relaxed">
-                Starting from web development both in front end and back end, I've explored various things in relation with web technologies. I can do DevOps, managing Linux servers, working with AWS, and being a part as your company's infrastructure engineering team.
-              </p>
-              <p className="mb-8 leading-relaxed">Not only that, I can do game development as well as desktop application developments. I made games using Unity and love to experiment with low-level programming.</p>
-              
-            </div>
+import { DotLottiePlayer, Controls } from "@dotlottie/react-player";
 
-        </div>
-    )
-}
+const About: React.FC = () => {
+  return (
+    <div className="container mx-auto flex flex-col justify-center py-20 lg:py-40 px-10 h-screen">
+      <div className="lg:flex-grow md:pr-16 md:items-start md:text-left mb-16 md:mb-0 text-center flex flex-col h-full self-center justify-center">
+        <h1 className="sm:text-6xl mb-4 font-medium text-white text-center">
+          "There are just so much possibilities in life that I am just too
+          excited to try them all."
+        </h1>
+        <p className="text-2xl text-right self-end">- Me</p>
+        {/* <DotLottiePlayer
+          src="/lottiefiles/anim1.lottie"
+          autoplay
+          loop
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        ></DotLottiePlayer> */}
+      </div>
+    </div>
+  );
+};
 
 export default About;
