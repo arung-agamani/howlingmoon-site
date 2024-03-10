fca10ee64ebf995865d8c3ee7a3639826d87bf0c
Arung Agamani <arung.agamani@gmail.com>
2024-03-10 19:04:08 +0800

    chore: import gltf

--- src/routes/playground.lazy.tsx
+++ src/routes/playground.lazy.tsx
@@ -2,56 +2,44 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import Box from "../components/three-objects/Box";
 
 import "./playground.css";
 import { Scroll, ScrollControls } from "@react-three/drei";
-import Torus from "../components/three-objects/Torus";
-import { Color } from "three";
+import { Model as CustomModel } from "../components/three-objects/Test1";
 
 const Playground = () => {
     return (
         <div id="canvas-container" className="w-full h-screen">
             <Suspense fallback={<span>Loading...</span>}>
-                <Canvas>
-                    <ambientLight intensity={Math.PI / 2} />
-                    <spotLight
-                        position={[20, 10, 10]}
-                        angle={0.15}
-                        penumbra={1}
-                        decay={0}
-                        intensity={Math.PI}
-                    />
-                    <pointLight
-                        position={[-10, -10, -10]}
-                        decay={0}
-                        intensity={Math.PI}
-                    />
-                    {/* <Box position={[-1.2, 0, 0]} rotateDir="x" />
-                    <Box position={[1.2, 0, 0]} rotateDir="x" />
-                    <Box position={[0, 1, 0]} rotateDir="y" />
-                    <Box position={[0, -1, 0]} rotateDir="y" /> */}
-                    <Torus
-                        torusProps={{ args: [5, 1, 4, 6] }}
-                        rotateDir="z"
-                        rotateSpeed={0.2}
-                        materialProps={{
-                            color: new Color(0x603cf0),
-                        }}
-                    />
-                    <Torus
-                        torusProps={{ args: [2, 0.4, 4, 5] }}
-                        rotateDir="z"
-                        rotateSpeed={-0.6}
-                        materialProps={{
-                            emissive: new Color(0x5a945c),
-                            color: new Color(0x0fb815),
-                        }}
-                    />
+                <Canvas shadows>
+                    <group
+                        rotation={[Math.PI * 0.2, Math.PI * 0.25, 0]}
+                        position={[0, 0.3, 0]}
+                        scale={[1.2, 1.2, 1.2]}
+                    >
+                        <ambientLight intensity={Math.PI / 2} />
+                        <spotLight
+                            position={[10, 10, 10]}
+                            angle={0.15}
+                            penumbra={1}
+                            decay={0}
+                            intensity={Math.PI}
+                            castShadow
+                        />
+                        <pointLight
+                            position={[-10, -10, -10]}
+                            decay={0}
+                            intensity={Math.PI}
+                            castShadow
+                        />
+                        <CustomModel position={[0, -4, 0]} />
+                    </group>
                     <ScrollControls pages={1} damping={0}>
                         <Scroll html>
                             <section className="w-screen h-screen flex justify-center items-center">
-                                <h2 className="text-3xl">~~Awoo~~</h2>
+                                <h2 className="text-3xl text-white">
+                                    ~~Awoo~~
+                                </h2>
                             </section>
                         </Scroll>
                     </ScrollControls>
